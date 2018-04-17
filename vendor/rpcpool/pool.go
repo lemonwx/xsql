121f39828eff646673daf3736cb1b16a02d5c405
lim <wxgwmmoc@126.com>
2018-04-17 21:53:58 +0800

    add: rpc client pool

---
+++ vendor/rpcpool/pool.go
@@ -0,0 +1,72 @@
+/**
+ *  author: lim
+ *  data  : 18-4-17 下午9:50
+ */
+
+ /*
+  	build in rpc client's pool based on redis/pool
+  */
+
+package rpcpool
+
+import (
+	"time"
+	"sync"
+	"net/rpc"
+)
+
+type poolConn struct {
+	c          rpc.Client
+	t          time.Time
+	created    time.Time
+	next, prev *poolConn
+}
+
+type idleList struct {
+	count       int
+	front, back *poolConn
+}
+
+type Pool struct {
+	// Dial is an application supplied function for creating and configuring a
+	// connection.
+	//
+	// The connection returned from Dial must not be in a special state
+	// (subscribed to pubsub channel, transaction started, ...).
+	Dial func() (rpc.Client, error)
+
+	// TestOnBorrow is an optional application supplied function for checking
+	// the health of an idle connection before the connection is used again by
+	// the application. Argument t is the time that the connection was returned
+	// to the pool. If the function returns an error, then the connection is
+	// closed.
+	TestOnBorrow func(c rpc.Client, t time.Time) error
+
+	// Maximum number of idle connections in the pool.
+	MaxIdle int
+
+	// Maximum number of connections allocated by the pool at a given time.
+	// When zero, there is no limit on the number of connections in the pool.
+	MaxActive int
+
+	// Close connections after remaining idle for this duration. If the value
+	// is zero, then idle connections are not closed. Applications should set
+	// the timeout to a value less than the server's timeout.
+	IdleTimeout time.Duration
+
+	// If Wait is true and the pool is at the MaxActive limit, then Get() waits
+	// for a connection to be returned to the pool before returning.
+	Wait bool
+
+	// Close connections older than this duration. If the value is zero, then
+	// the pool does not close connections based on age.
+	MaxConnLifetime time.Duration
+
+	chInitialized uint32 // set to 1 when field ch is initialized
+
+	mu     sync.Mutex    // mu protects the following fields
+	closed bool          // set to true when the pool is closed.
+	active int           // the number of open connections in the pool
+	ch     chan struct{} // limits open connections when p.Wait is true
+	idle   idleList      // idle connections
+}
