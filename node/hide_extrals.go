4193679b51d9801515c6df3b0e96fa012e3e4b1b
lim <wxgwmmoc@126.com>
2018-06-11 21:18:58 +0800

    fix bug: stmt execute hide extra col should adjust base on extra col size

--- node/hide_extrals.go
+++ node/hide_extrals.go
@@ -14,11 +14,13 @@ import (
 	"github.com/lemonwx/xsql/mysql"
 )
 
+var UNEXPECTED_NODE_ERR = errors.New("UNEXPECTED NODE ERROR")
+
 func (node *Node) hideExtraCols(rs *mysql.Result, data *[]byte, vs map[uint64]uint8) error {
 	log.Debugf("[%d] hide extra cols: %v", node.ConnectionId, data)
 
 	if node.IsStmt {
-		pos := 1 + (len(rs.Fields)+1+7+2)>>3
+		pos := 1 + (len(rs.Fields)+node.ExtraSize+7+2)>>3
 		nullMask := (*data)[1:pos]
 		if ((nullMask[(0+2)>>3] >> uint((0+2)&7)) & 1) == 1 {
 			return errors.New("UNEXPECT VERSION IS NULL")
@@ -26,6 +28,10 @@ func (node *Node) hideExtraCols(rs *mysql.Result, data *[]byte, vs map[uint64]ui
 		start_pos := pos
 
 		for idx := 0; idx < node.ExtraSize; idx += 1 {
+			if ((nullMask[(idx+2)>>3] >> uint((idx+2)&7)) & 1) == 1 {
+				log.Errorf("[%d] unexpected node err: version parsed from ret is nil", node.ConnectionId)
+				return UNEXPECTED_NODE_ERR
+			}
 			extra := uint64(binary.LittleEndian.Uint64((*data)[pos : pos+8]))
 			log.Debugf("[%d] extra col val: %v", node.ConnectionId, extra)
 			if _, ok := vs[extra]; ok {
