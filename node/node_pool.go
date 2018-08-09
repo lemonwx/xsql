9b82092f4548bf64f786f67e618f08867befe723
lim <wxgwmmoc@126.com>
2018-08-09 22:42:54 +0800

    add: pool.NewConn

--- node/node_pool.go
+++ node/node_pool.go
@@ -5,12 +5,7 @@
 
 package node
 
-import (
-	"fmt"
-
-	"github.com/lemonwx/xsql/config"
-	"github.com/lemonwx/xsql/errors"
-)
+import "github.com/lemonwx/xsql/config"
 
 const (
 	MaxInitFailedSize = 5
@@ -20,37 +15,49 @@ type Pool struct {
 	adminConn   *Node
 	idleChan    chan *Node
 	maxConnSize uint32
+
+	host     string
+	port     int
+	user     string
+	password string
+}
+
+func (p *Pool) NewConn() (*Node, error) {
+	conn := NewNode(p.host, p.port, p.user, p.password, "", 0)
+	if err := conn.Connect(); err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
 
 func NewNodePool(initSize, idleSize, maxConnSize uint32, cfg *config.Node) (*Pool, error) {
-	m := &Pool{}
-	m.idleChan = make(chan *Node, idleSize)
-	m.maxConnSize = maxConnSize
+	p := &Pool{
+		maxConnSize: maxConnSize,
+		host:        cfg.Host,
+		port:        cfg.Port,
+		user:        cfg.User,
+		password:    cfg.Password,
+	}
 
-	node := NewNode(cfg.Host, cfg.Port, cfg.User, cfg.Password, "", 0)
-	if err := node.Connect(); err != nil {
+	if conn, err := p.NewConn(); err != nil {
 		return nil, err
 	} else {
-		m.adminConn = node
+		p.adminConn = conn
 	}
 
 	failedSize := 0
+	p.idleChan = make(chan *Node, idleSize)
 	for count := uint32(0); count < initSize; {
-		node := NewNode(cfg.Host, cfg.Port, cfg.User, cfg.Password, "", 0)
-		if err := node.Connect(); err != nil {
+		if conn, err := p.NewConn(); err != nil {
 			failedSize++
-			if failedSize >= MaxInitFailedSize {
-				err := errors.New(fmt.Errorf("init conn failed too many times"))
-				return nil, err
-			}
 		} else {
-			failedSize = 0
-			m.idleChan <- node
+			p.idleChan <- conn
 			count++
+			failedSize++
 		}
 	}
 
-	return m, nil
+	return p, nil
 }
 
 func (pool *Pool) GetConn() *Node {
