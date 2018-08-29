64cf9765c44dfd7a21d063da01fadab6f2e7b1f3
lim <wxgwmmoc@126.com>
2018-08-29 20:53:22 +0800

    modify: handle admin cmd by reflect

--- server/admin.go
+++ server/admin.go
@@ -8,7 +8,9 @@ package server
 import (
 	"fmt"
 
-	"time"
+	"reflect"
+
+	"strconv"
 
 	"github.com/lemonwx/log"
 	"github.com/lemonwx/xsql/mysql"
@@ -22,7 +24,6 @@ const (
 
 func (conn *MidConn) handleAdmin(stmt *sqlparser.Admin, sql string) error {
 	log.Debugf("[%d] handle admin command: %s", conn.ConnectionId, sql)
-	log.Debug(stmt.Name, stmt.Values)
 
 	switch string(stmt.Name) {
 	case UseVersions:
@@ -55,39 +56,50 @@ func (conn *MidConn) handleAdmin(stmt *sqlparser.Admin, sql string) error {
 		return conn.writeResultset(conn.status, rs)
 	case TimeStat:
 		rs := &mysql.Resultset{
-			Fields:     []*mysql.Field{{Name: []byte("phase")}, {Name: []byte("cost")}},
-			FieldNames: map[string]int{"phase": 0, "cost": 1},
+			Fields: []*mysql.Field{
+				{Name: []byte("phase")},
+				{Name: []byte("cost")},
+				{Name: []byte("count")},
+				{Name: []byte("avg")},
+			},
+			FieldNames: map[string]int{
+				"phase": 0, "cost": 1, "counts": 2, "avg": 3},
 		}
 
-		phases := map[string]time.Duration{"routeT": 0, "sqlparseT": 0, "versionT": 0, "chkInuseT": 0, "execT": 0}
-		rs.RowDatas = make([]mysql.RowData, 0, len(phases))
+		ret := newStat()
+		t := reflect.TypeOf(*ret)
+		v := reflect.ValueOf(*ret)
 
-		for _, conn := range conn.svr.cos.midConns {
-			phases["routeT"] += conn.stat.routeT
-			phases["sqlparseT"] += conn.stat.sqlparseT
-			phases["versionT"] += conn.stat.versionT
-			phases["chkInuseT"] += conn.stat.chkInuseT
-			phases["execT"] += conn.stat.execT
+		for _, co := range conn.svr.cos.midConns {
+			sVal := reflect.ValueOf(*co.stat)
+			for i := 0; i < t.NumField(); i++ {
+				sField := sVal.Field(i).Interface().(*field)
+				tField := v.Field(i).Interface().(*field)
+				tField.t += sField.t
+				tField.c += sField.c
+			}
 		}
 
-		for _, stat := range conn.svr.stats {
-			phases["routeT"] += stat.routeT
-			phases["sqlparseT"] += stat.sqlparseT
-			phases["versionT"] += stat.versionT
-			phases["chkInuseT"] += stat.chkInuseT
-			phases["execT"] += stat.execT
-		}
+		rs.RowDatas = make([]mysql.RowData, 0, t.NumField()+1)
+		for i := 0; i < t.NumField(); i++ {
+			phase := t.Field(i).Name
+			stat := v.Field(i).Interface().(*field)
+			row := make([]byte, 0, len(phase)*4)
 
-		for _, phase := range []string{"execT", "versionT", "sqlparseT", "routeT", "chkInuseT"} {
-			t := phases[phase]
-			lens := len(phase)
-			row := make([]byte, 0, lens*2)
-			row = append(row, byte(lens))
-			row = append(row, []byte(phase)...)
-			cost := t.String()
-			row = append(row, byte(len(cost)))
-			row = append(row, []byte(cost)...)
+			row = append(row, byte(len(phase)))
+			row = append(row, phase...)
 
+			t := stat.t.String()
+			row = append(row, byte(len(t)))
+			row = append(row, t...)
+
+			c := strconv.FormatInt(stat.c, 10)
+			row = append(row, byte(len(c)))
+			row = append(row, c...)
+
+			avg := stat.avg().String()
+			row = append(row, byte(len(avg)))
+			row = append(row, avg...)
 			rs.RowDatas = append(rs.RowDatas, row)
 		}
 		return conn.writeResultset(conn.status, rs)
