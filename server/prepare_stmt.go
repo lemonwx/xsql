5b6a182fecc067a20d9c6939970ffd8220f2057e
lim <wxgwmmoc@126.com>
2018-09-29 21:54:07 +0800

    rm unused prepare stmt code

--- server/prepare_stmt.go
+++ server/prepare_stmt.go
@@ -6,8 +6,6 @@
 package server
 
 import (
-	"fmt"
-
 	"encoding/binary"
 
 	"sync"
@@ -613,76 +611,4 @@ func newMyStmt(s sqlparser.Statement, co *MidConn) (myStmt, error) {
 		return nil, newMySQLErr(errUnsupportedSql)
 	}
 
-}
-
-type Stmt struct {
-	id uint32
-
-	cliParams  int
-	nodeParams int
-
-	cliColumns  uint16
-	nodeColumns uint16
-
-	cliArgs  []interface{}
-	nodeArgs []interface{}
-
-	s sqlparser.Statement
-
-	sql       string
-	originSql string
-
-	stmtIdMeta map[int]uint32
-
-	//forUpdateStmts []*Stmt
-	forUpdateStmts  map[int]*Stmt
-	forUpStmtIdMeta map[int]uint32
-	forUpdateSql    string
-
-	updateStmts      map[int]*Stmt
-	updateStmtIdMeta map[int]uint32
-	updateSql        string
-
-	firstPrepare bool
-}
-
-func NewStmt() *Stmt {
-	stmt := new(Stmt)
-	stmt.stmtIdMeta = make(map[int]uint32)
-
-	stmt.forUpdateStmts = make(map[int]*Stmt)
-	stmt.forUpStmtIdMeta = make(map[int]uint32)
-
-	stmt.updateStmtIdMeta = make(map[int]uint32)
-	stmt.updateStmts = make(map[int]*Stmt)
-
-	stmt.firstPrepare = true
-
-	return stmt
-}
-
-func (s *Stmt) InitParams() {
-	s.cliArgs = make([]interface{}, s.cliParams)
-	s.nodeArgs = make([]interface{}, s.nodeParams)
-}
-
-func (s *Stmt) ResetParams(size int) {
-	for idx := 0; idx < s.cliParams; idx += 1 {
-		s.cliArgs[idx] = nil
-	}
-	for idx := 0; idx < s.nodeParams; idx += 1 {
-		s.nodeArgs[idx] = nil
-	}
-}
-
-func (s *Stmt) ChkEqual(params int, columns uint16) error {
-	if s.firstPrepare {
-		return fmt.Errorf("should not call ChkEqual at first prepare")
-	} else {
-		if s.nodeParams == params && s.nodeColumns == columns {
-			return nil
-		}
-	}
-
-	return fmt.Errorf("exec prepare between multi nodes ret not equal")
 }
